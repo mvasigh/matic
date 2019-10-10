84bc4e26191b3ef15ed2848853a9398a7f9c0042
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-10-10 07:49:41 -0500

    remove dep and add basic script using tagged literal

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-const applescript = require('applescript');
+const exec = require('child_process').exec
+const applescript = require('./src/lib/applescript')
+
 // TODO: set up commander
+
+const app = 'Google Chrome'
+
+const script = applescript`
+  say "Hello world"
+  tell application "${app}"
+    reopen
+    activate
+  end tell
+  delay 0.5
+  tell application "iTerm"
+    reopen
+    activate
+  end tell
+  delay 0.5
+  tell application "System Events"
+    key code 123 using {control down}
+    delay 0.5
+    key code 124 using {control down}
+    delay 0.5
+  end tell
+  say "This was a fun little applescript!"
+`
+
+exec(`osascript ${script}`)
