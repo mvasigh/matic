4d3423e3e51af5635d83c7ddf136ea2857aed1fd
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-10-10 07:49:50 -0500

    add a test

--- src/lib/__tests__/applescript.js
+++ src/lib/__tests__/applescript.js
@@ -1,11 +1,23 @@
-const applescript = require('../applescript')
+const applescript = require('../applescript');
 
-test('it should return a string', () => {
-  const application = 'Google Chrome'
-  const script = applescript`
+const LINE_REGEX = /-e "([^"]|\\")*"/g;
+
+describe('applescript tagged template literal', () => {
+  it('should return a string', () => {
+    const application = 'Google Chrome';
+    const script = applescript`
     tell application "${application}"
-      activate
+    activate
     end tell
-  `
-  expect(typeof script).toEqual('string')
+    `;
+    expect(typeof script).toEqual('string');
+  });
+
+  it('should split by line', () => {
+    const script = applescript`
+      abc
+      1234
+    `;
+    expect(script.match(LINE_REGEX).length).toEqual(2);
+  });
 });
